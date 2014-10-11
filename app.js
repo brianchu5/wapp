5ecec1a03f4509425131eeb4b57389101b110b1e
Brian <bchu521@gmail.com>
2014-10-11 14:22:55 -0700

    send

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var main = function() {
   "use strict";
   $(".comment-input button").on("click", function (event) {
-      var $new_comment = $("<p>"),
+      var $new_comment;
       
   if ($(".comment-input input").val() !== "" ){
-  comment_text = $(".comment-input input").val();
+  var comment_text = $(".comment-input input").val();
 
-        $new_comment.text(comment_text);
+        $new_comment=$("<p>").text(comment_text);
      $(".comments").append($new_comment);
+     $(".comment-input input").val("");
+     $(".comment-input input).on("keypress", function (event) {
+         
 }
    });
  };
